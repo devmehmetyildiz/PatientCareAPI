d5f7896a0f9873cda98c09b35873f84df5bd6b75
Mehmet Yildiz <yildiz655@gmail.com>
2022-06-19 23:51:57 +0300

    Web[2.4] : New Roles added

--- PatientCareAPI/Models/Authentication/UserAuthory.cs
+++ PatientCareAPI/Models/Authentication/UserAuthory.cs
@@ -15,7 +15,68 @@ namespace PatientCareAPI.Models.Authentication
         public const string User_Add = "Add_User";
         public const string User_Update = "User_Update";
         public const string User_Delete = "User_Delete";
-      
-        
+        public const string User_ManageAll = "User_Delete";
+
+        public const string Department_Screen = "Department_Screen";
+        public const string Department_Add = "Department_Add";
+        public const string Department_Update = "Department_Update";
+        public const string Department_Delete = "Department_Delete";
+        public const string Department_ManageAll = "Department_ManageAll";
+
+        public const string Stock_Screen = "Stock_Screen";
+        public const string Stock_Add = "Stock_Add";
+        public const string Stock_Update = "Stock_Update";
+        public const string Stock_Delete = "Stock_Delete";
+        public const string Stock_ManageAll = "Stock_ManageAll";
+
+        public const string Patients_Screen = "Patients_Screen";
+        public const string Patients_Add = "Patients_Add";
+        public const string Patients_Update = "Patients_Update";
+        public const string Patients_Delete = "Patients_Delete";
+        public const string Patients_ManageAll = "Patients_ManageAll";
+        public const string Patient_UploadFile = "Patient_UploadFile";
+        public const string Patient_DownloadFile = "Patient_DownloadFile";
+        public const string Patient_ViewFile = "Patient_ViewFile";
+
+        public const string Patienttype_Screen = "Patienttype_Screen";
+        public const string Patienttype_Add = "Patienttype_Add";
+        public const string Patienttype_Update = "Patienttype_Update";
+        public const string Patienttype_Delete = "Patienttype_Delete";
+        public const string Patienttype_ManageAll = "Patienttype_ManageAll";
+
+        public const string Unit_Screen = "Unit_Screen";
+        public const string Unit_Add = "Unit_Add";
+        public const string Unit_Update = "Unit_Update";
+        public const string Unit_Delete = "Unit_Delete";
+        public const string Unit_ManageAll = "Unit_ManageAll";
+
+        public const string Case_Screen = "Case_Screen";
+        public const string Case_Add = "Case_Add";
+        public const string Case_Update = "Case_Update";
+        public const string Case_Delete = "Case_Delete";
+        public const string Case_ManageAll = "Case_ManageAll";
+
+        public const string Roles_Screen = "Roles_Screen";
+        public const string Roles_Add = "Roles_Add";
+        public const string Roles_Update = "Roles_Update";
+        public const string Roles_Delete = "Roles_Delete";
+        public const string Roles_ManageAll = "Roles_ManageAll";
+
+        public const string Dashboard_AllScreen = "Dashboard_AllScreen";
+        public const string Dashboard_DepartmentScreen = "Dashboard_DepartmentScreen";
+
+        public const string Unittype_Screen = "Unittype_Screen";
+        public const string Unittype_Add = "Unittype_Add";
+        public const string Unittype_Update = "Unittype_Update";
+        public const string Unittype_Delete = "Unittype_Delete";
+        public const string Unittype_ManageAll = "Unittype_ManageAll";
+
+        public const string Reminding_Screen = "Reminding_Screen";
+        public const string Reminding_Add = "Reminding_Add";
+        public const string Reminding_Update = "Reminding_Update";
+        public const string Reminding_Delete = "Reminding_Delete";
+        public const string Reminding_ManageAll = "Reminding_ManageAll";
+        public const string Reminding_DefineforAll = "Reminding_DefineforAll";
+        public const string Reminding_Define = "Reminding_Define";
     }
 }
