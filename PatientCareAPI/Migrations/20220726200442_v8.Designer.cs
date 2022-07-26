de86001f1d9f5ec684d3b874fe86e76c61a7469b
Mehmet Yildiz <yildiz655@gmail.com>
2022-07-26 23:52:27 +0300

    Web[5.2] : Active stock migration updated

--- PatientCareAPI/Migrations/20220726200442_v8.Designer.cs
+++ PatientCareAPI/Migrations/20220726200442_v8.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PatientCareAPI.DataAccess;
 
 namespace PatientCareAPI.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220726200442_v8")]
+    partial class v8
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PatientCareAPI/Migrations/20220726200442_v8.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace PatientCareAPI.Migrations
+{
+    public partial class v8 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Activestock",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Stockid = table.Column<string>(type: "text", nullable: true),
+                    Skt = table.Column<string>(type: "text", nullable: true),
+                    Barcodeno = table.Column<string>(type: "text", nullable: true),
+                    Amount = table.Column<double>(type: "double", nullable: false),
+                    Purchaseprice = table.Column<double>(type: "double", nullable: false),
+                    Purchasedate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Info = table.Column<string>(type: "text", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "varchar(85)", maxLength: 85, nullable: true),
+                    CreatedUser = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    UpdatedUser = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    DeleteUser = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    CreateTime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    UpdateTime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    DeleteTime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    IsActive = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activestock", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Activestock");
+        }
+    }
+}
