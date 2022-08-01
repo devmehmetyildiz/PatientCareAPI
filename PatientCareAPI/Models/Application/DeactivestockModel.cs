83897bad493dd956167616f2c17c2beee37fcade
deadmen107 <yildiz655@gmail.com>
2022-08-01 17:57:06 +0300

    Web[5.4] : New movement models added

---
+++ PatientCareAPI/Models/Application/DeactivestockModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientCareAPI.Models.Application
+{
+    public class DeactivestockModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Activestockid { get; set; }
+        public DateTime? Createtime { get; set; }
+        public string Createduser { get; set; }
+        public string Amount { get; set; }
+    }
+}
---
+++ PatientCareAPI/Models/Application/StockmovementModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientCareAPI.Models.Application
+{
+    public class StockmovementModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [NotMapped]
+        public string Stockname { get; set; }
+        public string Activestockid { get; set; }
+        public string UserID { get; set; }
+        [NotMapped]
+        public string Movementtypename { get; set; }
+        public int Movementtype { get; set; }
+        public double Amount { get; set; }
+        public double Prevvalue { get; set; }
+        public double Newvalue { get; set; }
+        public DateTime? Movementdate { get; set; }
+    
+    }
+}
